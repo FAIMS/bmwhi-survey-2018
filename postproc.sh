7ea81ed1b771cac9032044e0683bf0b934d4786e
DoxasticFox <doxasticfox@github.com>
2018-03-16 19:02:50 +1100

    BMS2018-2

---
+++ postproc.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+cd module
+
+string="
+        <select1 ref=\"Entity_Types\">
+          <label>\{Entity_Types}<\/label>
+          <item>
+            <label>Options not loaded<\/label>
+            <value>Options not loaded<\/value>
+          <\/item>
+        <\/select1>"
+replacement="
+        <group ref=\"Colgroup_1\" faims_style=\"orientation\">
+          <label\/>
+          <group ref=\"Col_0\" faims_style=\"even\">
+            <label\/>
+            <select1 ref=\"Entity_Types\">
+              <label>\{Entity_Types}<\/label>
+              <item>
+                <label>Options not loaded<\/label>
+                <value>Options not loaded<\/value>
+              <\/item>
+            <\/select1>
+          <\/group>
+          <group ref=\"Col_1\" faims_style=\"even\">
+            <label\/>
+            <select1 ref=\"Select_User\">
+              <label>\{Select_User}<\/label>
+              <item>
+                <label>Options not loaded<\/label>
+                <value>Options not loaded<\/value>
+              <\/item>
+            <\/select1>
+          <\/group>
+          <group ref=\"Col_2\" faims_style=\"even\">
+            <label\/>
+            <select1 ref=\"Feature_Type\">
+              <label>\{Feature_Type}<\/label>
+              <item>
+                <label>Options not loaded<\/label>
+                <value>Options not loaded<\/value>
+              <\/item>
+            <\/select1>
+          <\/group>
+        <\/group>"
+perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
+
+string="
+              <Entity_Types\/>"
+replacement="
+              <Colgroup_1>
+                <Col_0>
+                  <Entity_Types\/>
+                <\/Col_0>
+                <Col_1>
+                  <Select_User\/>
+                <\/Col_1>
+                <Col_2>
+                  <Feature_Type\/>
+                <\/Col_2>
+              <\/Colgroup_1>"
+perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
+
+string="ref=\"Feature_ID\" faims_style_class=\"required\""
+replacement="ref=\"Feature_ID\" faims_style_class=\"required-readonly\""
+perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
+
+string="ref=\"Photographed_Feature_ID\" faims_style_class=\"required\""
+replacement="ref=\"Photographed_Feature_ID\" faims_style_class=\"required-readonly\""
+perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
+
+cat << EOF >> english.0.properties
+Select_User=Select User
+EOF
+
+cat << EOF >> ui_styling.css
+.orange {
+  background-color: orange;
+}
+.required-readonly {
+  color: #B2B2B2;
+}
+.required-readonly-label {
+  color: red;
+}
+EOF
+
+rm ui_schema.xml.original
---
+++ tests/ModuleUtil.java
@@ -0,0 +1,465 @@
+package au.org.intersect.faims.android.util;
+
+import android.view.View;
+import android.widget.EditText;
+
+import com.robotium.solo.Solo;
+
+public class ModuleUtil extends ModuleHelper {
+    /*
+        Type: tab group
+        Ref:  Control
+     */
+    public static View get_Control(Solo solo) {
+        String ref = "Control";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab group
+        Ref:  Feature
+     */
+    public static View get_Feature(Solo solo) {
+        String ref = "Feature";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab group
+        Ref:  Login
+     */
+    public static View get_Login(Solo solo) {
+        String ref = "Login";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab group
+        Ref:  Photolog
+     */
+    public static View get_Photolog(Solo solo) {
+        String ref = "Photolog";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+    /*
+        Type: tab
+        Ref:  Control/Control
+     */
+    public static View get_Control_Control(Solo solo) {
+        String ref = "Control/Control";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab
+        Ref:  Control/Defaults
+     */
+    public static View get_Control_Defaults(Solo solo) {
+        String ref = "Control/Defaults";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab
+        Ref:  Feature/Associated_Material
+     */
+    public static View get_Feature_AssociatedMaterial(Solo solo) {
+        String ref = "Feature/Associated_Material";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab
+        Ref:  Feature/External_Photos
+     */
+    public static View get_Feature_ExternalPhotos(Solo solo) {
+        String ref = "Feature/External_Photos";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab
+        Ref:  Feature/Feature
+     */
+    public static View get_Feature_Feature(Solo solo) {
+        String ref = "Feature/Feature";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab
+        Ref:  Login/User_List
+     */
+    public static View get_Login_UserList(Solo solo) {
+        String ref = "Login/User_List";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: tab
+        Ref:  Photolog/Photolog
+     */
+    public static View get_Photolog_Photolog(Solo solo) {
+        String ref = "Photolog/Photolog";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+    /*
+        Type: button
+        Ref:  Control/Control/New_Feature
+     */
+    public static View get_Control_Control_NewFeature(Solo solo) {
+        String ref = "Control/Control/New_Feature";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Control/Control/New_Photolog
+     */
+    public static View get_Control_Control_NewPhotolog(Solo solo) {
+        String ref = "Control/Control/New_Photolog";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Control/Search/Search_Button
+     */
+    public static View get_Control_Search_SearchButton(Solo solo) {
+        String ref = "Control/Search/Search_Button";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Feature/Associated_Material/Picture_of_Associated_Material_Button_1
+     */
+    public static View get_Feature_AssociatedMaterial_PictureofAssociatedMaterialButton1(Solo solo) {
+        String ref = "Feature/Associated_Material/Picture_of_Associated_Material_Button_1";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Feature/Feature/Attach_Photo_Button_1
+     */
+    public static View get_Feature_Feature_AttachPhotoButton1(Solo solo) {
+        String ref = "Feature/Feature/Attach_Photo_Button_1";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Feature/Feature/CHECK_FOR_MISSING_DATA
+     */
+    public static View get_Feature_Feature_CHECKFORMISSINGDATA(Solo solo) {
+        String ref = "Feature/Feature/CHECK_FOR_MISSING_DATA";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Feature/Feature/Photo_of_Sketch_Button_1
+     */
+    public static View get_Feature_Feature_PhotoofSketchButton1(Solo solo) {
+        String ref = "Feature/Feature/Photo_of_Sketch_Button_1";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: button
+        Ref:  Feature/Feature/Take_From_GPS_1
+     */
+    public static View get_Feature_Feature_TakeFromGPS1(Solo solo) {
+        String ref = "Feature/Feature/Take_From_GPS_1";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: camera
+        Ref:  Feature/Associated_Material/Picture_of_Associated_Material
+     */
+    public static View get_Feature_AssociatedMaterial_PictureofAssociatedMaterial(Solo solo) {
+        String ref = "Feature/Associated_Material/Picture_of_Associated_Material";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: camera
+        Ref:  Feature/Feature/Attach_Photo
+     */
+    public static View get_Feature_Feature_AttachPhoto(Solo solo) {
+        String ref = "Feature/Feature/Attach_Photo";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: camera
+        Ref:  Feature/Feature/Photo_of_Sketch
+     */
+    public static View get_Feature_Feature_PhotoofSketch(Solo solo) {
+        String ref = "Feature/Feature/Photo_of_Sketch";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: checkbox
+        Ref:  Feature/Associated_Material/Associated_Feature_Type
+     */
+    public static View get_Feature_AssociatedMaterial_AssociatedFeatureType(Solo solo) {
+        String ref = "Feature/Associated_Material/Associated_Feature_Type";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: checkbox
+        Ref:  Feature/Associated_Material/Associated_Material
+     */
+    public static View get_Feature_AssociatedMaterial_AssociatedMaterial(Solo solo) {
+        String ref = "Feature/Associated_Material/Associated_Material";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: dropdown
+        Ref:  Control/Search/Entity_Types
+     */
+    public static View get_Control_Search_EntityTypes(Solo solo) {
+        String ref = "Control/Search/Entity_Types";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: dropdown
+        Ref:  Feature/Feature/Feature_Type
+     */
+    public static View get_Feature_Feature_FeatureType(Solo solo) {
+        String ref = "Feature/Feature/Feature_Type";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Control/Defaults/Next_Feature_ID
+     */
+    public static EditText get_Control_Defaults_NextFeatureID(Solo solo) {
+        String ref = "Control/Defaults/Next_Feature_ID";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Control/Search/Search_Term
+     */
+    public static EditText get_Control_Search_SearchTerm(Solo solo) {
+        String ref = "Control/Search/Search_Term";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Associated_Material/Asociated_Material_Notes
+     */
+    public static EditText get_Feature_AssociatedMaterial_AsociatedMaterialNotes(Solo solo) {
+        String ref = "Feature/Associated_Material/Asociated_Material_Notes";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/External_Photos/External_Photo_Notes
+     */
+    public static EditText get_Feature_ExternalPhotos_ExternalPhotoNotes(Solo solo) {
+        String ref = "Feature/External_Photos/External_Photo_Notes";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/External_Photos/IDs_of_Pictures_from_External_Camera
+     */
+    public static EditText get_Feature_ExternalPhotos_IDsofPicturesfromExternalCamera(Solo solo) {
+        String ref = "Feature/External_Photos/IDs_of_Pictures_from_External_Camera";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Feature/Accuracy
+     */
+    public static View get_Feature_Feature_Accuracy(Solo solo) {
+        String ref = "Feature/Feature/Accuracy";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Feature/Description
+     */
+    public static EditText get_Feature_Feature_Description(Solo solo) {
+        String ref = "Feature/Feature/Description";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Feature/Easting
+     */
+    public static View get_Feature_Feature_Easting(Solo solo) {
+        String ref = "Feature/Feature/Easting";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Feature/Feature_ID
+     */
+    public static EditText get_Feature_Feature_FeatureID(Solo solo) {
+        String ref = "Feature/Feature/Feature_ID";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Feature/Height_m
+     */
+    public static EditText get_Feature_Feature_Heightm(Solo solo) {
+        String ref = "Feature/Feature/Height_m";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Feature/Latitude
+     */
+    public static View get_Feature_Feature_Latitude(Solo solo) {
+        String ref = "Feature/Feature/Latitude";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Feature/Length_m
+     */
+    public static EditText get_Feature_Feature_Lengthm(Solo solo) {
+        String ref = "Feature/Feature/Length_m";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Feature/Longitude
+     */
+    public static View get_Feature_Feature_Longitude(Solo solo) {
+        String ref = "Feature/Feature/Longitude";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Feature/Northing
+     */
+    public static View get_Feature_Feature_Northing(Solo solo) {
+        String ref = "Feature/Feature/Northing";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Feature/Other_Dimension_Annotation
+     */
+    public static EditText get_Feature_Feature_OtherDimensionAnnotation(Solo solo) {
+        String ref = "Feature/Feature/Other_Dimension_Annotation";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Feature/Tag_Name
+     */
+    public static EditText get_Feature_Feature_TagName(Solo solo) {
+        String ref = "Feature/Feature/Tag_Name";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Feature/Feature/Width_m
+     */
+    public static EditText get_Feature_Feature_Widthm(Solo solo) {
+        String ref = "Feature/Feature/Width_m";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Photolog/Photolog/External_Photo_Notes
+     */
+    public static EditText get_Photolog_Photolog_ExternalPhotoNotes(Solo solo) {
+        String ref = "Photolog/Photolog/External_Photo_Notes";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Photolog/Photolog/IDs_of_Picture_from_External_Camera
+     */
+    public static EditText get_Photolog_Photolog_IDsofPicturefromExternalCamera(Solo solo) {
+        String ref = "Photolog/Photolog/IDs_of_Picture_from_External_Camera";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: input
+        Ref:  Photolog/Photolog/Photographed_Feature_ID
+     */
+    public static EditText get_Photolog_Photolog_PhotographedFeatureID(Solo solo) {
+        String ref = "Photolog/Photolog/Photographed_Feature_ID";
+        return (android.widget.EditText) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: list
+        Ref:  Control/Control/List_of_Existing_Features
+     */
+    public static View get_Control_Control_ListofExistingFeatures(Solo solo) {
+        String ref = "Control/Control/List_of_Existing_Features";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: list
+        Ref:  Control/Search/Entity_List
+     */
+    public static View get_Control_Search_EntityList(Solo solo) {
+        String ref = "Control/Search/Entity_List";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: list
+        Ref:  Login/User_List/Users
+     */
+    public static View get_Login_UserList_Users(Solo solo) {
+        String ref = "Login/User_List/Users";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: radio
+        Ref:  Feature/Feature/Feature_Identification_Certainty
+     */
+    public static View get_Feature_Feature_FeatureIdentificationCertainty(Solo solo) {
+        String ref = "Feature/Feature/Feature_Identification_Certainty";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+
+    /*
+        Type: radio
+        Ref:  Feature/Feature/Recommendation
+     */
+    public static View get_Feature_Feature_Recommendation(Solo solo) {
+        String ref = "Feature/Feature/Recommendation";
+        return (android.view.View) solo.getView((Object) ref);
+    }
+}
